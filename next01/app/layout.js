06f0f4a4a8c1090860cc9e89cc5772ddc8aab161
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-01 12:53:53 +0600

    Pages & Layout

--- next01/app/layout.js
+++ next01/app/layout.js
@@ -11,7 +11,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="p-4 border-b border-gray-400">
+          <ul className="flex gap-5">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
