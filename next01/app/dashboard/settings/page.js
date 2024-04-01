b3455e8f550e0cafad73d16d6e825a4e6cc3b7a4
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-01 12:33:16 +0600

    route

---
+++ next01/app/dashboard/settings/page.js
@@ -0,0 +1,13 @@
+export default function Settings() {
+  return (
+    <div>
+      <h1 className="text-2xl">Hello, Next.js!</h1>
+      <p className="mt-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nam
+        numquam quo possimus fugiat soluta? Qui sit, beatae quod iusto esse
+        dignissimos aperiam quo, sapiente quos eveniet vero, quibusdam
+        praesentium?
+      </p>
+    </div>
+  );
+}
