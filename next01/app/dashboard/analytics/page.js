081bc5a23581578dd0929ae845e77c1d0a924a92
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-01 13:53:39 +0600

    Pages & Layout

--- next01/app/dashboard/analytics/page.js
+++ next01/app/dashboard/analytics/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Analytics Page",
+  description: "This is our analytics page",
+};
+
 export default function Analytics() {
   return (
     <div className="p-4">
