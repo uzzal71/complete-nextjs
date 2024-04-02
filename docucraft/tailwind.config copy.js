117280b21202c3f7e6448803fdd681635b33bed5
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-03 01:48:04 +0600

    docucraft

--- docucraft/tailwind.config copy.js
+++ docucraft/tailwind.config copy.js
@@ -9,11 +9,11 @@ module.exports = {
   theme: {
     fontSize: {
       "2xs": ["0.75rem", { lineHeight: "1.25rem" }],
-      xs: ["0.8125rem", { lineHeight: "1.5rem" }],
-      sm: ["0.875rem", { lineHeight: "1.5rem" }],
-      base: ["1rem", { lineHeight: "1.75rem" }],
-      lg: ["1.125rem", { lineHeight: "1.75rem" }],
-      xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      "xs": ["0.8125rem", { lineHeight: "1.5rem" }],
+      "sm": ["0.875rem", { lineHeight: "1.5rem" }],
+      "base": ["1rem", { lineHeight: "1.75rem" }],
+      "lg": ["1.125rem", { lineHeight: "1.75rem" }],
+      "xl": ["1.25rem", { lineHeight: "1.75rem" }],
       "2xl": ["1.5rem", { lineHeight: "2rem" }],
       "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
       "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
