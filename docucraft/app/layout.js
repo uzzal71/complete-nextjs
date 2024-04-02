b84234116d8d2aae591dd5dff24d69cbfc537709
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-03 00:44:53 +0600

    docucraft

--- docucraft/app/layout.js
+++ docucraft/app/layout.js
@@ -1,3 +1,4 @@
+import { getDocuments } from "@/lib/doc";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +10,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const allDocuments = getDocuments();
+  console.log(allDocuments);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
