ac645e970b832071512552c28a6636bc6db3bb27
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-04 01:33:58 +0600

    docucraft

--- docucraft/components/Sidebar.jsx
+++ docucraft/components/Sidebar.jsx
@@ -1,12 +1,63 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Sidebar({ docs }) {
-  const roots = docs.filter((doc) => !doc.parent);
+import {
+  getDocumentsByAuthor,
+  getDocumentsByCategory,
+  getDocumentsByTag,
+} from "@/utils/doc-util";
 
-  const nonRoots = Object.groupBy(
-    docs.filter((doc) => doc.parent),
-    ({ parent }) => parent
-  );
+const Sidebar = ({ docs }) => {
+  const pathName = usePathname();
+  const [rootNodes, setRootNodes] = useState([]);
+  const [nonRootNodesGrouped, setNonRootNodesGrouped] = useState({});
+
+  useEffect(() => {
+    let matchedDocs = docs;
+
+    if (pathName.includes("/tags")) {
+      const tag = pathName.split("/")[2];
+      matchedDocs = getDocumentsByTag(docs, tag);
+    } else if (pathName.includes("/authors")) {
+      const author = pathName.split("/")[2];
+      matchedDocs = getDocumentsByAuthor(docs, author);
+    } else if (pathName.includes("/categories")) {
+      const category = pathName.split("/")[2];
+      matchedDocs = getDocumentsByCategory(docs, category);
+    }
+
+    const roots = matchedDocs.filter((post) => !post.parent);
+
+    const nonRoots = Object.groupBy(
+      matchedDocs.filter((post) => post.parent),
+      ({ parent }) => parent
+    );
+
+    const nonRootsKeys = Reflect.ownKeys(nonRoots);
+    nonRootsKeys.forEach((key) => {
+      const foundInRoots = roots.find((root) => root.id === key);
+      if (!foundInRoots) {
+        const foundInDocs = docs.find((doc) => doc.id === key);
+        roots.push(foundInDocs);
+      }
+    });
+
+    roots.sort((a, b) => {
+      if (a.order < b.order) {
+        return -1;
+      }
+      if (a.order > b.order) {
+        return 1;
+      }
+      return 0;
+    });
+
+    setRootNodes([...roots]);
+    setNonRootNodesGrouped({ ...nonRoots });
+  }, [pathName]);
 
   return (
     <nav className="lg:block my-10">
@@ -16,7 +67,7 @@ export default function Sidebar({ docs }) {
           <div className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
           <div className="absolute left-2 h-6 w-px bg-emerald-500"></div>
           <ul role="list" className="border-l border-transparent">
-            {roots.map((rootNode) => (
+            {rootNodes.map((rootNode) => (
               <li key={rootNode.id} className="relative">
                 <Link
                   aria-current="page"
@@ -25,9 +76,9 @@ export default function Sidebar({ docs }) {
                 >
                   <span className="truncate">{rootNode.title}</span>
                 </Link>
-                {nonRoots[rootNode.id] && (
+                {nonRootNodesGrouped[rootNode.id] && (
                   <ul role="list" className="border-l border-transparent">
-                    {nonRoots[rootNode.id].map((subRoot) => (
+                    {nonRootNodesGrouped[rootNode.id].map((subRoot) => (
                       <li key={subRoot.id}>
                         <Link
                           className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
@@ -46,4 +97,6 @@ export default function Sidebar({ docs }) {
       </ul>
     </nav>
   );
-}
+};
+
+export default Sidebar;
