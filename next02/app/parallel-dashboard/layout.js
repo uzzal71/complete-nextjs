6b4304a49fba26f8c0215b772bfa582c4e1a5504
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-05 12:38:48 +0600

    next02

--- next02/app/parallel-dashboard/layout.js
+++ next02/app/parallel-dashboard/layout.js
@@ -1,17 +1,15 @@
 export default function DashboardLayout({
   children,
   assignments,
-  quiz,
   notifications,
+  quiz,
 }) {
-  const isLoggedIn = false;
-
   return (
     <>
       {children}
       <div className="grid gap-6 grid-cols-2 grid-rows-2 p-8">
         {assignments}
-        {isLoggedIn && notifications}
+        {notifications}
         {quiz}
       </div>
     </>
