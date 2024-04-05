08841c66a01b4a68868978c2927f0184e2258181
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-05 13:15:24 +0600

    next02

--- next02/app/parallel-dashboard/@assignments/page.js
+++ next02/app/parallel-dashboard/@assignments/page.js
@@ -1,8 +1,4 @@
-import wait from "@/app/lib/wait";
-
 export default async function AssignmentsPage() {
-  await wait(2000);
-
   return (
     <div className="text-xl p-4 border border-gray-200 rounded h-[360px] flex items-center justify-center">
       ASSIGNMENTS
--- next02/app/parallel-dashboard/@quiz/page.js
+++ next02/app/parallel-dashboard/@quiz/page.js
@@ -1,8 +1,4 @@
-import wait from "@/app/lib/wait";
-
 export default async function Quiz() {
-  await wait(2000);
-
   return (
     <div className="text-xl p-4 border border-gray-200 rounded h-[360px] flex items-center justify-center">
       QUIZ
