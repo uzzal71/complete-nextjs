c47a3c88ae701496dc05245e885c499d4abe7fbb
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-05 12:24:15 +0600

    next02

--- next02/app/components/Header.js
+++ next02/app/components/Header.js
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="p-6 text-center border-b border-gray-400">
+      <h1 className="font-bold text-xl">HEADER</h1>
+    </div>
+  );
 }
--- next02/app/components/Notifications.js
+++ next02/app/components/Notifications.js
@@ -1,3 +1,7 @@
 export default function Notifications() {
-  return <div>Notifications</div>;
+  return (
+    <div className="text-xl p-4 row-span-2 border border-gray-200 rounded h-[745px] flex items-center justify-center">
+      NOTIFICATIONS
+    </div>
+  );
 }
