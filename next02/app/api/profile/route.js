ca99deae8c248cba093a2ea8ce1509d3d8626395
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-06 00:37:00 +0600

    next02

--- next02/app/api/profile/route.js
+++ next02/app/api/profile/route.js
@@ -1,6 +1,10 @@
+import { headers } from "next/headers";
+
 export async function GET(request) {
-  const requestHeaders = new Headers(request.headers);
-  console.log(requestHeaders);
+  //   const requestHeaders = new Headers(request.headers);
+  //   console.log(requestHeaders.get("Authorization"));
+  const headerList = headers();
+  console.log(headerList.get("Authorization"));
 
   return new Response("Profile API");
 }
