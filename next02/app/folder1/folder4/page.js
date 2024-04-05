a5c46447b9d091e4c13046e3c08f969b4cdab5ab
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-05 14:08:12 +0600

    next02

---
+++ next02/app/folder1/folder4/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Folder4() {
+  return (
+    <div className="p-20 flex-col gap-10">
+      <h1 className="text-2xl">Folder 4</h1>
+      <Link href="/folder1/folder3" className="text-blue-400">
+        Folder 3
+      </Link>
+    </div>
+  );
+}
