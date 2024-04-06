c77f71fd268da04644ab0d9daa3dc1d115b98a4a
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-07 01:06:55 +0600

    photofeed

--- photofeed/app/[lang]/page.js
+++ photofeed/app/[lang]/page.js
@@ -1,8 +1,8 @@
-import { getDictionary } from "./disctionaries";
+import PhotoList from "@/components/PhotoList";
 
-export default async function Home({params: {lang}}) {
-  const dictionary = await getDictionary(lang);
-  return (
-    <div>{dictionary.followers}</div>
-  );
+export default async function Home() {
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json();
+
+  return <PhotoList photos={photos} />;
 }
