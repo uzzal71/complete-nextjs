98bc6ddfe36c2c22a9ca89bdbb50ee5e46c38e4a
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-14 23:52:00 +0600

    next

--- next03/app/page.js
+++ next03/app/page.js
@@ -1,4 +1,9 @@
-export default function Home() {
+import getJoke from "@/utils/getJoke";
+import RandJoke from "./components/RandomJoke";
+
+export default async function Home() {
+  const joke = await getJoke();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-5">
       <h1 className="text-xl">{joke.value}</h1>
