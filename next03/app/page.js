c9277a72553095934eba7b0dfd7d18bd1e871977
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-15 23:13:43 +0600

    next03

--- next03/app/page.js
+++ next03/app/page.js
@@ -1,5 +1,5 @@
 // import getJoke from "@/utils/getJoke";
-// import RandJoke from "./components/RandomJoke";
+import RandJoke from "./components/RandomJoke";
 
 export default async function Home() {
   // const joke = await getJoke();
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-5">
       {/* <h1 className="text-xl">{joke.value}</h1> */}
-      {/* <RandJoke /> */}
+      <RandJoke />
       <h1>Data Fetching, caching & revalidating</h1>
     </main>
   );
