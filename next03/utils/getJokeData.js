bec9c2090a4af01bb897eebce4491a3bffbf8b2c
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-17 23:16:01 +0600

    data fetch

--- next03/utils/getJokeData.js
+++ next03/utils/getJokeData.js
@@ -1,5 +1,5 @@
 export default async function getJoke() {
-  const res = await fetch("https://api.chucknorris.io/jokes/random");
+  const res = await fetch("https://api.chucknorris.io/jokes/random", {});
 
   if (!res.ok) {
     throw new Error("Fetch error...");
