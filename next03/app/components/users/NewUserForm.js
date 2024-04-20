bac1e1c5cb67f08d8dd2293ab7a6d1058a5de096
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-20 19:30:42 +0600

    server action

--- next03/app/components/users/NewUserForm.js
+++ next03/app/components/users/NewUserForm.js
@@ -3,7 +3,6 @@ import { addUser } from "@/actions/user";
 export default function NewUserForm() {
   return (
     <form action={addUser}>
-      <input type="hidden" name="flag" value="pro" />
       <div>
         <input type="text" name="name" placeholder="Name" />
       </div>
