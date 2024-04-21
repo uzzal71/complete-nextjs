838ac7129ce551bc382026c0c36937dfa1f1f0be
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-21 23:18:22 +0600

    update

--- next03/models/Product.js
+++ next03/models/Product.js
@@ -1,3 +1,8 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
