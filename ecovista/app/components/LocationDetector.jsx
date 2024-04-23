8f1c5886e80bee3db2f403db6b18a639b5315f8e
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-23 14:06:00 +0600

    ecovista

--- ecovista/app/components/LocationDetector.jsx
+++ ecovista/app/components/LocationDetector.jsx
@@ -1,6 +1,28 @@
 "use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function LocationDetector() {
+  const [loading, setLoading] = useState(false);
+
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    setLoading(true);
+    const params = new URLSearchParams(searchParams);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        params.set("latitude", position.coords.latitude);
+        params.set("longitude", position.coords.longitude);
+        setLoading(false);
+        router.push(`/current?${params.toString()}`);
+      });
+    }
+  }, [pathName, searchParams]);
+
   return <div>LocationDetector</div>;
 }
 
