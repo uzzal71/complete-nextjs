3fe796756382162062f90c680637bc57d2fcaee1
uzzal71 <uzzalroy.acm@gmail.com>
2024-04-24 14:41:18 +0600

    font optimization

--- next_font_optimization/app/components/users/User.js
+++ next_font_optimization/app/components/users/User.js
@@ -1,7 +1,7 @@
 import { getUserByEmail } from "@/utils/getUserByEmail";
 
 export default async function User() {
-    const user = await getUserByEmail("saad@learnwithsumit.com");
+  const user = await getUserByEmail("uzzalroy.acm@gmail.com");
 
-    return <div className="box-3 text-xl text-slate-800">{user.name}</div>;
+  return <div className="box-3 text-xl text-slate-800">{user.name}</div>;
 }
